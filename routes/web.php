b07b3965cca8486f0d6b1ccc988af7aa421cca57
eDceL <undeadban.emp@gmail.com>
2021-02-16 11:25:36 +0800

    add view step increment

--- routes/web.php
+++ routes/web.php
@@ -10,27 +10,16 @@ Route::get('/', function () {
     return view('blank-page');
 });
 
-
-// Route::get('/{any}', function ($any) {
-//     $view = str_replace('.html', '', $any);
-//     return view($view);
-// });
-
 Route::view('/view-layouts', function() {
    return view('activities');
 });
 
+
+//salary grade
 Route::Resource('/salary-grade', 'SalaryGradeController');
 Route::get('/salary-grade-list', 'SalaryGradecontroller@list');
-
+//plantilla
 Route::Resource('/plantilla', 'PlantillaController');
 Route::get('/plantilla-list', 'Plantillacontroller@list');
-
-
-// Route::view('/SalaryGrade.AddSalaryGrade', 'SalaryGrade.AddSalaryGrade');
-// Route::post('/salary_grade', 'SalaryGradecontroller@store')->name('save');
-// Route::get('/salary_grade', 'SalaryGradecontroller@index');
-// Route::get('/list', 'SalaryGradecontroller@list');
-// Route::view('/step_increment', 'stepincrement');
-// Route::view('/plantilla' , 'plantilla');
-// Route::get('/list_plantilla', 'PlantillaController@list');
+//step Increment
+Route::Resource('/step-increment', 'StepIncrementController');
