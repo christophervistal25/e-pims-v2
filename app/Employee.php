e09c2df1a7aaa7b58906a430a983abf599c3d1c3
undeadban-emp <undeadban.emp@gmail.com>
2021-02-24 09:23:37 +0800

    remove employee information model all columns merge to employee model

--- app/Employee.php
+++ app/Employee.php
@@ -10,9 +10,46 @@ class Employee extends Model
     protected $primaryKey = 'employee_id';
 
     protected $fillable = [
-        'employee_id', 'old_item_no','lastname','firstname','middlename','sex','employee_ids','date_birth','status',
+        'employee_id',
+        'lastname',
+        'firstname',
+        'middlename',
+        'extension',
+        'date_birth',
+        'place_birth',
+        'sex',
+        'civil_status',
+        'height',
+        'weight',
+        'blood_type',
+        'gsis_id_no',
+        'pag_ibig_no',
+        'philhealth_no',
+        'sss_no',
+        'tin_no',
+        'agency_employee_no',
+        'citizenship',
+        'residential_house_no',
+        'residential_street',
+        'residential_village',
+        'residential_barangay',
+        'residential_city',
+        'residential_province',
+        'permanent_house_no',
+        'permanent_street',
+        'permanent_village',
+        'permanent_barangay',
+        'permanent_city',
+        'permanent_province',
+        'telephone_no',
+        'mobile_no',
+        'email_address',
     ];
 
+
+    /**
+     * Generate unique Employee I.D for every employee
+     */
     public static function boot()
     {
         parent::boot();
@@ -22,13 +59,33 @@ class Employee extends Model
     }
 
 
+    /**
+     * Set the firstname of employee to uppercase
+     */
+    public function setFirstNameAttribute($value)
+    {
+        $this->attributes['firstname'] = strtoupper($value);
+    }
+
+    public function setMiddleNameAttribute($value)
+    {
+        $this->attributes['middlename'] = strtoupper($value);
+    }
+
+    public function setLastNameAttribute($value)
+    {
+        $this->attributes['lastname'] = strtoupper($value);
+    }
+
+    public function setSuffixAttribute($value)
+    {
+        $this->attributes['suffix'] = strtoupper($value);
+    }
+
+
     public function plantilla()
     {
         return $this->hasMany(Plantilla::class);
     }
 
-    public function information()
-    {
-        return $this->hasOne(EmployeePersonalInformation::class);
-    }
 }
