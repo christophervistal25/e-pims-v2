5614df5466c6ac158aab158ae4670ef1df330151
undeadban-emp <undeadban.emp@gmail.com>
2021-02-26 11:38:42 +0800

    add form layout for c3 tab => Voluntary, Learning and development , other information

--- resources/views/PersonalData/create.blade.php
+++ resources/views/PersonalData/create.blade.php
@@ -8,8 +8,8 @@
 <div class="card">
     <div class="card-body">
         <ul class="nav nav-tabs nav-tabs-solid nav-justified">
-            <li class="nav-item"><a class="nav-link " href="#c1-tab" data-toggle="tab">C1</a></li>
-            <li class="nav-item"><a class="nav-link active" href="#c2-tab" data-toggle="tab">C2</a></li>
+            <li class="nav-item"><a class="nav-link active" href="#c1-tab" data-toggle="tab">C1</a></li>
+            <li class="nav-item"><a class="nav-link" href="#c2-tab" data-toggle="tab">C2</a></li>
             <li class="nav-item"><a class="nav-link" href="#c3-tab" data-toggle="tab">C3</a></li>
             <li class="nav-item"><a class="nav-link" href="#c4-tab" data-toggle="tab">C4</a></li>
         </ul>
@@ -27,7 +27,7 @@
                         </h5>
                       </div>
 
-                      <div id="personalInformation" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                      <div id="personalInformation" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
                         <div class="card-body">
                           Personal Information
                         </div>
@@ -65,154 +65,14 @@
                 {{-- END OF ACCORDION FOR C1 --}}
             </div>
             {{-- END OF C1 --}}
-            <div class="tab-pane show active" id="c2-tab">
-                {{-- BEGIN OF ACCORDION FOR C2 --}}
-                <div id="accordion">
-                    <div class="card">
-                      <div class="card-header" id="headingOne">
-                        <h5 class="mb-0">
-                          <button class="btn btn-link" data-toggle="collapse" data-target="#civilService" aria-expanded="true" aria-controls="civilService">
-                            IV. Civil Service Egibility
-                          </button>
-                        </h5>
-                      </div>
-
-                      <div id="civilService" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-                        <div class="card-body">
-                          {{-- BEGIN CONTENT OF CIVIL SERVICE --}}
-                          <table class="table table-bordered">
-                            <tr class="text-center" style="background: #EAEAEA;">
-                              <td rowspan="2" class="align-middle">27. CAREER SERVICE/ RA 1080 (BOARD/ BAR) UNDER SPECIAL LAWS/ CES/ CSEE BARANGAY ELIGIBILITY / DRIVER'S LICENSE</td>
-                              <td rowspan="2" class="align-middle">RATING</td>
-                              <td rowspan="2" class="align-middle">DATE OF EXAMINATION / CONFERMENT</td>
-                              <td rowspan="2" class="align-middle">PLACE OF EXAMINATION / CONFERMENT</td>
-                              <td colspan="2" scope="colgroup">LICENSE</td>
-                              <td rowspan="2" class="pl-4 pr-4">&nbsp;</td>
-                            </tr>
-                            <tr style="background: #EAEAEA;">
-                                <td scope="col" class="text-center">NUMBER</td>
-                                <td scope="col" class="text-center">Date of Validity</td>
-                            </tr>
-
-
-                            {{-- BEGIN OF DATA --}}
-                            <tbody id="dynamic-row-civil-service-data">
-                                <tr>
-                                    <th scope="row">
-                                      <input type="text" class="form-control rounded-0 border-0" placeholder="Input here...">
-                                    </th>
-                                    <td>
-                                      <input type="number" class="form-control rounded-0 border-0" placeholder="e.g. 91.2">
-                                    </td>
-                                    <td>
-                                        <input type="date" class="form-control rounded-0 border-0" placeholder="Input">
-                                    </td>
-                                    <td>
-                                        <input type="text" class="form-control rounded-0 border-0" placeholder="e.g Tandag">
-                                    </td>
-
-                                    <td>
-                                        <input type="text" class="form-control rounded-0 border-0" placeholder="e.g. 2015">
-                                    </td>
-                                    <td>
-                                        <input type="text" class="form-control rounded-0 border-0" placeholder="e.g. 2016">
-                                    </td>
-                                    <td class="jumbotron"></td>
-                                </tr>
-                            </tbody>
-                            {{-- END OF DATA --}}
-                          </table>
-                          <div class="float-right mb-3">
-                              <button class="btn btn-primary" id="btnAddNewFieldCivilServiceRow">
-                                  <i class="fa fa-plus"></i>
-                                  Add new field
-                                </button>
-                          </div>
-                          {{-- END CONTENT OF CIVIL SERVICE --}}
-                        </div>
-                      </div>
-                    </div>
-                    <div class="card">
-                      <div class="card-header" id="headingTwo">
-                        <h5 class="mb-0">
-                          <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#workExperience" aria-expanded="false" aria-controls="workExperience">
-                            V. Work Experience
-                          </button>
-                        </h5>
-                      </div>
-
-                      <div id="workExperience" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-                        <div class="card-body">
-                            {{-- BEGIN CONTENT OF WORK EXPERIENCE --}}
-                            <table class="table table-bordered">
-                                <tr class="text-center" style="background: #EAEAEA;">
-                                  <td colspan="2" scope="colgroup">28. INCLUSIVE DATES (mm/dd/yyyy)</td>
-                                  <td rowspan="2" class="align-middle">POSITION TITLE (Write in full/Do not abbreviate)</td>
-                                  <td rowspan="2" class="align-middle">DEPARTMENT / AGENCY / OFFICE / COMPANY (Write in full/Do not abbreviate)</td>
-                                  <td rowspan="2" class="align-middle">MONTHLY SALARY</td>
-                                  <td rowspan="2" class="align-middle">SALARY/ JOB/ PAY GRADE <br> (if applicable) <br> & STEP  (Format "00-0")/ INCREMENT</td>
-                                  <td rowspan="2" class="align-middle">STATUS OF APPOINTMENT</td>
-                                  <td rowspan="2" class="align-middle">GOV'T SERVICE (Y/ N)</td>
-                                  <td rowspan="2" class="pl-4 pr-4">&nbsp;</td>
-                                </tr>
-
-                                <tr style="background: #EAEAEA;">
-                                  <td scope="col" class="text-center">FROM</td>
-                                  <td scope="col" class="text-center">TO</td>
-                                </tr>
-
-
-                                {{-- BEGIN OF DATA --}}
-                                <tbody id="dynamic-row-work-experience-data">
-                                    <tr>
-                                        <th scope="row">
-                                          <input type="date" class="form-control rounded-0 border-0" placeholder="FROM">
-                                        </th>
-                                        <td>
-                                          <input type="date" class="form-control rounded-0 border-0" placeholder="TO">
-                                        </td>
-                                        <td>
-                                            <input type="text" class="form-control rounded-0 border-0" placeholder="Input">
-                                        </td>
-                                        <td>
-                                            <input type="text" class="form-control rounded-0 border-0" placeholder="e.g Tandag">
-                                        </td>
-
-                                        <td>
-                                            <input type="text" class="form-control rounded-0 border-0" placeholder="">
-                                        </td>
-                                        <td>
-                                            <input type="text" class="form-control rounded-0 border-0" placeholder="">
-                                        </td>
-                                        <td>
-                                            <input type="text" class="form-control rounded-0 border-0" placeholder="">
-                                        </td>
-                                        <td>
-                                            <input type="text" class="form-control rounded-0 border-0" placeholder="">
-                                        </td>
-                                        <td class="jumbotron"></td>
-                                    </tr>
-                                </tbody>
-                                {{-- END OF DATA --}}
-                              </table>
-                              <div class="float-right mb-3">
-                                  <button class="btn btn-primary" id="btnAddNewWorkExperienceRow">
-                                      <i class="fa fa-plus"></i>
-                                      Add new field
-                                    </button>
-                              </div>
-                            {{-- END CONTENT OF WORK EXPERIENCE --}}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                {{-- END OF ACCORDION FOR C2 --}}
+            <div class="tab-pane" id="c2-tab">
+                @include('PersonalData.sections.C2')
             </div>
-            <div class="tab-pane" id="c3-tab">
-                Tab content 3
+            <div class="tab-pane show active" id="c3-tab">
+                @include('PersonalData.sections.C3')
             </div>
             <div class="tab-pane" id="c4-tab">
-                Tab content 4
+                @include('PersonalData.sections.C4')
             </div>
         </div>
     </div>
@@ -220,10 +80,13 @@
 
 @push('page-scripts')
 <script>
-    let civilServiceDynamicRowIndex = 0;
-    let workExperienceDynamicRowIndex = 0;
+    let civilServiceDynamicRowIndex           = 0;
+    let workExperienceDynamicRowIndex         = 0;
+    let voluntaryDynamicRowIndex              = 0;
+    let learningAndDevelopmentDynamicRowIndex = 0;
+    let otherInformationDynamicRowIndex = 0;
 
-    // This will add new table row for civil service
+    // This will add new table row for civil service in C2 TAB
     $('#btnAddNewFieldCivilServiceRow').click((e) => {
         civilServiceDynamicRowIndex++;
         let rowID = `row-cilvil-service-${civilServiceDynamicRowIndex}`;
@@ -258,7 +121,7 @@
     });
 
 
-    // Add new table row for work experience
+    // Add new table row for work experience in C2 TAB
     $('#btnAddNewWorkExperienceRow').click((e) => {
         workExperienceDynamicRowIndex++;
         let rowID = `row-work-experience-${workExperienceDynamicRowIndex}`
@@ -290,6 +153,92 @@
                     <input type="text" class="form-control rounded-0 border-0" placeholder="">
                 </td>
                 <td class='align-middle'>
+                    <button class="btn btn-sm rounded-pill btn-danger align-middle text-center" data-target="${rowID}">
+                        <i class='fa fa-times' style="pointer-events :none;"></i>
+                    </button>
+                </td>
+            </tr>
+        `);
+    });
+
+    // Add new table row for voluntary in C3 tab
+    $('#btnAddNewFieldVoluntaryRow').click((e) => {
+        voluntaryDynamicRowIndex++;
+        let rowID = `row-voluntary-${voluntaryDynamicRowIndex}`;
+        $( "#dynamic-row-voluntary-data" ).append(`
+            <tr id="${rowID}">
+                <th scope="row">
+                    <input type="text" class="form-control rounded-0 border-0" placeholder="NAME">
+                </th>
+                <td>
+                    <input type="date" class="form-control rounded-0 border-0" placeholder="">
+                </td>
+                <td>
+                    <input type="date" class="form-control rounded-0 border-0" placeholder="TO">
+                </td>
+                <td>
+                    <input type="numbe" class="form-control rounded-0 border-0" placeholder="Hours">
+                </td>
+                <td>
+                    <input type="text" class="form-control rounded-0 border-0" placeholder="Position">
+                </td>
+                <td class='align-middle'>
+                    <button class="btn btn-sm rounded-pill btn-danger align-middle text-center" data-target="${rowID}">
+                        <i class='fa fa-times' style="pointer-events :none;"></i>
+                    </button>
+                </td>
+            </tr>
+        `);
+    });
+
+     // Add new table row for learning and development in C3 tab
+    $('#btnAddNewFieldLearningAndDevelopmentRow').click((e) => {
+        learningAndDevelopmentDynamicRowIndex++;
+        let rowID = `row-learning-and-development-${learningAndDevelopmentDynamicRowIndex}`;
+        $('#dynamic-row-learning-and-development-data').append(`
+            <tr id="${rowID}">
+                <td>
+                    <input type="text" class="form-control rounded-0 border-0" placeholder="NAME">
+                </td>
+                <td>
+                    <input type="date" class="form-control rounded-0 border-0" placeholder="FROM">
+                </td>
+                <td>
+                    <input type="date" class="form-control rounded-0 border-0" placeholder="TO">
+                </td>
+                <td>
+                    <input type="number" class="form-control rounded-0 border-0" placeholder="Hours">
+                </td>
+                <td>
+                    <input type="text" class="form-control rounded-0 border-0" placeholder="">
+                </td>
+                <td>
+                <input type="text" class="form-control rounded-0 border-0" placeholder="">
+                </td>
+                <td>
+                    <button class="btn btn-sm rounded-pill btn-danger align-middle text-center" data-target="${rowID}">
+                        <i class='fa fa-times' style="pointer-events :none;"></i>
+                    </button>
+                </td>
+            </tr>
+        `);
+    });
+
+    $('#btnAddNewFieldOtherInformation').click((e) => {
+        otherInformationDynamicRowIndex++;
+        let rowID = `row-other-information-${otherInformationDynamicRowIndex}`;
+        $('#dynamic-row-other-information-data').append(`
+            <tr id="${rowID}">
+                <td>
+                    <input type="text" class="form-control rounded-0 border-0" placeholder="Input">
+                </td>
+                <td>
+                    <input type="text" class="form-control rounded-0 border-0" placeholder="Input">
+                </td>
+                <td>
+                    <input type="text" class="form-control rounded-0 border-0" placeholder="Input">
+                </td>
+                <td>
                     <button class="btn btn-sm rounded-pill btn-danger align-middle text-center" data-target="${rowID}">
                         <i class='fa fa-times' style="pointer-events :none;"></i>
                     </button>
