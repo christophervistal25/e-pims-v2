32414426b0c96262a38a4ef73be81a43af2fe912
unknown <baron.paulajane@gmail.com>
2021-05-12 14:44:14 +0800

    add vue component for add extension name in PDS-C1

--- resources/js/components/Employee/BasicInformation.vue
+++ resources/js/components/Employee/BasicInformation.vue
@@ -38,6 +38,7 @@
             v-model="employee.middleName"
             class="form-control text-uppercase"
             style="outline: none; box-shadow: 0px 0px 0px transparent"
+            placeholder="Middle Name"
           />
           <span>MIDDLE NAME</span>
         </label>
