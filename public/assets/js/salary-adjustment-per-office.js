d1c27861ec6ff45f02c52071c552636d250030e0
eDcel <undeadban.emp@gmail.com>
2021-06-04 14:18:33 +0800

    added datatable in adjust salary of salary adjustment per office

--- public/assets/js/salary-adjustment-per-office.js
+++ public/assets/js/salary-adjustment-per-office.js
@@ -1,41 +1,25 @@
 $(function() {
     let table = $("#salaryAdjustmentPerOffice").DataTable({
-        // processing: true,
+        processing: true,
         serverSide: true,
         destroy: true,
         retrieve: true,
         paging: false,
         info: false,
         bFilter: false,
-        aoColumnDefs: [
-            {
-                orderable: false,
-                className: "d-none",
-                aTargets: [0],
-                width: "5%",
-                targets: 0
-            }
-        ],
-        order: [[1, "asc"]],
         ajax: {
-            url: "/salary-adjustment-per-office-list",
-            data: function(d) {
-                d.employeeOffice = $("#employeeOffice").val();
-            }
+            url: "/salary-adjustment-per-office-list"
         },
         columns: [
-            {
-                data: "checkbox",
-                name: "checkbox",
-                searchable: false,
-                orderable: false,
-                sortable: false
-                // visible: false
-            },
             { data: "employee", name: "employee", visible: false },
             {
                 data: "plantilla",
                 name: "plantilla.office_code",
+                visible: false
+            },
+            {
+                data: "date_adjustment",
+                name: "date_adjustment",
                 visible: false
             },
             { data: "sg_no", name: "sg_no", visible: false },
@@ -61,35 +45,19 @@ $(function() {
                 paging: false,
                 info: false,
                 bFilter: false,
-                aoColumnDefs: [
-                    {
-                        orderable: false,
-                        className: "",
-                        aTargets: [0],
-                        width: "5%",
-                        targets: 0
-                    }
-                ],
-                order: [[1, "asc"]],
                 ajax: {
-                    url: "/salary-adjustment-per-office-list",
-                    data: function(d) {
-                        d.employeeOffice = $("#employeeOffice").val();
-                    }
+                    url: "/salary-adjustment-per-office-list"
                 },
                 columns: [
-                    {
-                        data: "checkbox",
-                        name: "checkbox",
-                        searchable: false,
-                        orderable: false,
-                        sortable: false,
-                        visible: false
-                    },
                     { data: "employee", name: "employee", visible: false },
                     {
                         data: "plantilla",
                         name: "plantilla.office_code",
+                        visible: false
+                    },
+                    {
+                        data: "date_adjustment",
+                        name: "date_adjustment",
                         visible: false
                     },
                     { data: "sg_no", name: "sg_no", visible: false },
@@ -97,15 +65,26 @@ $(function() {
                     {
                         data: "salary_previous",
                         name: "salary_previous",
-                        visible: false
+                        visible: false,
+                        render: $.fn.dataTable.render.number(",", ".", 2)
                     },
-                    { data: "salary_new", name: "salary_new", visible: false },
+                    {
+                        data: "salary_new",
+                        name: "salary_new",
+                        visible: false,
+                        render: $.fn.dataTable.render.number(",", ".", 2)
+                    },
                     {
                         data: "salary_diff",
                         name: "salary_diff",
                         visible: false
                     },
-                    { data: "action", name: "action", visible: false }
+                    {
+                        data: "action",
+                        name: "action",
+                        visible: false,
+                        render: $.fn.dataTable.render.number(",", ".", 2)
+                    }
                 ]
             });
         } else {
@@ -115,77 +94,50 @@ $(function() {
                 serverSide: true,
                 destroy: true,
                 retrieve: true,
-                aoColumnDefs: [
-                    {
-                        orderable: false,
-                        className: "",
-                        aTargets: [0],
-                        width: "5%",
-                        targets: 0
-                    }
-                ],
                 ajax: {
-                    url: `/api/office/salary/adjustment/peroffice/${e.target.value}`,
-                    data: function(d) {
-                        d.employeeName = $("#employeeOffice").val();
-                    }
+                    url: `/api/office/salary/adjustment/peroffice/${e.target.value}`
                 },
                 columns: [
-                    {
-                        data: "checkbox",
-                        name: "checkbox",
-                        searchable: false,
-                        orderable: false,
-                        sortable: false
-                    },
                     { data: "employee", name: "employee", visible: true },
                     {
                         data: "plantilla",
                         name: "plantilla.office_code",
                         visible: false
                     },
+                    {
+                        data: "date_adjustment",
+                        name: "date_adjustment",
+                        visible: true
+                    },
                     { data: "sg_no", name: "sg_no", visible: true },
                     { data: "step_no", name: "step_no", visible: true },
                     {
                         data: "salary_previous",
                         name: "salary_previous",
-                        visible: true
+                        visible: true,
+                        render: $.fn.dataTable.render.number(",", ".", 2)
                     },
-                    { data: "salary_new", name: "salary_new", visible: true },
-                    { data: "salary_diff", name: "salary_diff", visible: true },
+                    {
+                        data: "salary_new",
+                        name: "salary_new",
+                        visible: true,
+                        render: $.fn.dataTable.render.number(",", ".", 2)
+                    },
+                    {
+                        data: "salary_diff",
+                        name: "salary_diff",
+                        visible: true,
+                        render: $.fn.dataTable.render.number(",", ".", 2)
+                    },
                     { data: "action", name: "action", visible: true }
                 ]
             });
         }
     });
-    $("#selectAll").click(function() {
-        // Get all rows with search applied
-        var rows = table.rows({ search: "applied" }).nodes();
-        // Check/uncheck checkboxes for all rows in the table
-        $('input[type="checkbox"]', rows).prop("checked", this.checked);
-    });
-
-    // Handle click on checkbox to set state of "Select all" control
-    $("#salaryAdjustmentPerOffice tbody").on(
-        "change",
-        'input[type="checkbox"]',
-        function() {
-            // If checkbox is not checked
-            if (!this.checked) {
-                var el = $("#selectAll").get(0);
-                // If "Select all" control is checked and has 'indeterminate' property
-                if (el && el.checked && "indeterminate" in el) {
-                    // Set visual state of "Select all" control
-                    // as 'indeterminate'
-                    el.indeterminate = true;
-                }
-            }
-        }
-    );
 });
 
 function ValidateDropDown(dd) {
-    $("#sample").removeClass("d-none");
+    // $("#sample").removeClass("d-none");
     //disable button
     var input = document.getElementById("addbutton");
     if (dd.value == "") {
@@ -216,14 +168,6 @@ $(document).ready(function() {
     });
 });
 
-let selectElement = document.querySelector("#employeeOffice");
-
-selectElement.addEventListener("click", function() {
-    alert("sdasd");
-    const result = document.querySelector(".result");
-    result.textContent = `You like ${event.target.value}`;
-});
-
 // get value office
 $(document).ready(function() {
     $("#employeeOffice").change(function(e) {
@@ -237,5 +181,29 @@ $(document).ready(function() {
         } else {
             $("#officeAdjustment").text("");
         }
+    });
+});
+
+$(function() {
+    let table = $("#salaryAdjustmentPerOfficeList").DataTable({
+        processing: true,
+        serverSide: true,
+        destroy: true,
+        retrieve: true,
+        ajax: {
+            url: "/salary-adjustment-per-office-not-selected-list"
+        },
+        columns: [
+            { data: "employee", name: "employee", visible: true },
+            { data: "position", name: "position", visible: true },
+            { data: "sg_no", name: "sg_no", visible: true },
+            { data: "step_no", name: "step_no", visible: true },
+            {
+                data: "salary_amount",
+                name: "salary_amount",
+                visible: true,
+                render: $.fn.dataTable.render.number(",", ".", 2)
+            }
+        ]
     });
 });
--- resources/views/SalaryAdjustmentPerOffice/SalaryAdjustmentPerOffice.blade.php
+++ resources/views/SalaryAdjustmentPerOffice/SalaryAdjustmentPerOffice.blade.php
@@ -22,20 +22,34 @@
     <div class="card-body">
         <div id="add" class="page-header  {{  count($errors->all())  !== 0 ?  '' : 'd-none' }}">
 
-            <div style='padding-bottom:50px;margin-right:-15px;' class="col-auto ml-auto">
+            <div style='padding-top:20px; padding-bottom:50px;margin-right:-15px;' class="col-auto ml-auto">
                 <button id="cancelbutton" class="btn submit-btn btn-primary float-right"><i class="fa fa-list"></i> Salary Adjustment List</button>
             </div>
             <div class="row">
             <div class="col-12">
                 <div class="alert alert-secondary text-center font-weight-bold" role="alert" ><a id="officeAdjustment"></a></div>
             </div>
+            <div class="col-12">
+            <table class="table table-bordered text-center" id="salaryAdjustmentPerOfficeList" style="width:100%;">
+                <thead>
+                <tr>
+                    <td scope="col" class="text-center font-weight-bold">Employee Name</td>
+                    <td scope="col" class="text-center font-weight-bold">Position</td>
+                    <td scope="col" class="text-center font-weight-bold">Salary Grade</td>
+                    <td scope="col" class="text-center font-weight-bold">Step Number</td>
+                    <td scope="col" class="text-center font-weight-bold">Previous Salary</td>
+                </tr>
+                </thead>
+            </table>
+        </div>
+        </div>
         </div>
         </div>
 
         <div id="table" class="page-header ">
             {{-- {{  count($errors->all()) == 0 ? '' : 'd-none' }} --}}
             <div class="row">
-                <div class="col-6 mb-2">
+                <div style="padding-left:35px;" class="col-5 mb-2">
                     <select value="" data-style="btn-primary text-white" class="form-control form-control-xs selectpicker {{ $errors->has('employeeOffice')  ? 'is-invalid' : ''}}"
                         name="employeeOffice" data-live-search="true" id="employeeOffice" data-size="5" onchange="ValidateDropDown(this)">
                         <option></option>
@@ -44,23 +58,23 @@
                         @endforeach
                         </select>
             </div>
-            <div class="col-6 mb-2">
-                    <div class="float-right">
+            <div class="col-7 mb-2">
+                    <div style="padding-right:20px;" class="float-right">
                         <button id="addbutton" class="btn btn-primary float-right" disabled><i class="fa fa-plus"></i> Adjust Salary</button>
                     </div>
                 </div>
             </div>
-            <div class="table" style="overflow-x:auto;">
+            <div style="padding-left:20px; padding-right:20px;" class="table" style="overflow-x:auto;">
                 <table class="table table-bordered text-center" id="salaryAdjustmentPerOffice"  style="width:100%;">
                     <thead>
                     <tr>
-                        <td style="margin-top:10px;" scope="col" id="sample" class="text-center font-weight-bold d-none"><input style='transform:scale(1.3); margin-top:18px;' name="selectAll" value="selectAll" id="selectAll" type="checkbox" /></td>
                         <td scope="col" class="text-center font-weight-bold">Employee Name</td>
                         <td scope="col" class="text-center font-weight-bold d-none">Office</td>
+                        <td scope="col" class="text-center font-weight-bold">Adjustment Date</td>
                         <td scope="col" class="text-center font-weight-bold">Salary Grade</td>
                         <td scope="col" class="text-center font-weight-bold">Step Number</td>
-                        <td scope="col" class="text-center font-weight-bold">Salary Previous</td>
-                        <td scope="col" class="text-center font-weight-bold">Salary New</td>
+                        <td scope="col" class="text-center font-weight-bold">Previous Salary</td>
+                        <td scope="col" class="text-center font-weight-bold">New Salary</td>
                         <td scope="col" class="text-center font-weight-bold">Salary Difference</td>
                         <td scope="col" class="text-center font-weight-bold">Action</td>
                     </tr>
