e98d6ba0a026ed32a7d9acc9e9eb7e9b60893461
CHRis <christophervistal25@gmail.com>
2021-06-07 16:47:44 +0800

    Change some local assets into CDN

--- resources/views/layouts/app-vue.blade.php
+++ resources/views/layouts/app-vue.blade.php
@@ -27,11 +27,8 @@
     <meta name="msapplication-TileImage" content="{{ asset('/assets/img/icons/ms-icon-144x144.png') }}">
     <meta name="theme-color" content="#ffffff">
     <title>@yield('title') | {{  config('app.name') }}</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-        crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('/assets/css/font-awesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset('/assets/css/line-awesome.min.css') }}">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
@@ -229,13 +226,13 @@
         </div>
         <!-- /Header -->
         <!-- Sidebar -->
-		<div class="sidebar" id="sidebar">
-			<div class="sidebar-inner slimscroll">
-				<div id="sidebar-menu" class="sidebar-menu">
-					<ul>
-						<li class="menu-title">
-							<span>Personal Information Module</span>
-						</li>
+        <div class="sidebar" id="sidebar">
+            <div class="sidebar-inner slimscroll">
+                <div id="sidebar-menu" class="sidebar-menu">
+                    <ul>
+                        <li class="menu-title">
+                            <span>Personal Information Module</span>
+                        </li>
                         <li>
                             <a class='text-decoration-none' href="{{ route('employee.index') }}">
                                 <i class="la la-users"></i> <span> Employees </span>
@@ -246,67 +243,73 @@
                                 <i class="la la-file-text"></i> <span> Personal Data Sheet </span>
                             </a>
                         </li>
-						<li class="menu-title">
-							<span>Human Resource Module</span>
-						</li>
-						<li class="submenu">
-							<a href="#" class='text-decoration-none'><i class="la la-home"></i> <span> Leave Management </span> <span class="menu-arrow"></span></a>
-							<ul style="display: none;">
-								<li>
-                                    <a class='text-decoration-none mr-2' href="{{ route('leave.application.filling') }}">
+                        <li class="menu-title">
+                            <span>Human Resource Module</span>
+                        </li>
+                        <li class="submenu">
+                            <a href="#" class='text-decoration-none'><i class="la la-home"></i> <span> Leave Management
+                                </span> <span class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li>
+                                    <a class='text-decoration-none mr-2'
+                                        href="{{ route('leave.application.filling') }}">
                                         Leave Application Filling</a>
-                                    </li>
-								<li>
+                                </li>
+                                <li>
                                     <a class='text-decoration-none mr-2' href="#">
                                         Leave Recall, Cancel or Transfer
                                     </a>
                                 </li>
-								<li>
+                                <li>
                                     <a class='text-decoration-none mr-2' href="#">
                                         Leave Index Monitoring
                                     </a>
                                 </li>
 
-								<li>
+                                <li>
                                     <a class='text-decoration-none mr-2' href="#">
                                         Leave Starting Balance
                                     </a>
                                 </li>
-								<li>
+                                <li>
                                     <a class='text-decoration-none mr-2' href="#">
                                         Leave Forwaded Balance
                                     </a>
                                 </li>
-								<li>
+                                <li>
                                     <a class='text-decoration-none mr-2' href="#">
                                         Compensatory Build-up
                                     </a>
                                 </li>
-							</ul>
-						</li>
+                            </ul>
+                        </li>
 
 
-						<li class="submenu">
-							<a href="#" class='text-decoration-none'><i class="la la-bars"></i> <span>Service Record</span> <span class="menu-arrow"></span></a>
-							<ul style="display: none;">
-								<li>
+                        <li class="submenu">
+                            <a href="#" class='text-decoration-none'><i class="la la-bars"></i> <span>Service
+                                    Record</span> <span class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li>
                                     <a class='text-decoration-none mr-2' href="{{  route('service-records.index') }}">
                                         Maintenance & Monitoring
                                     </a>
                                 </li>
-							</ul>
-						</li>
+                            </ul>
+                        </li>
 
                         <li class="submenu">
-							<a href="#" class='text-decoration-none'><i class="la la-bookmark"></i> <span>Plantilla</span> <span class="menu-arrow"></span></a>
-							<ul style="display: none;">
-								<li>
-                                    <a class='text-decoration-none mr-2' href="{{  route('plantilla-of-position.index') }}">
+                            <a href="#" class='text-decoration-none'><i class="la la-bookmark"></i>
+                                <span>Plantilla</span> <span class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li>
+                                    <a class='text-decoration-none mr-2'
+                                        href="{{  route('plantilla-of-position.index') }}">
                                         Plantilla of Position
                                     </a>
                                 </li>
                                 <li>
-                                    <a class='text-decoration-none mr-2' href="{{  route('plantilla-of-personnel.index') }}">
+                                    <a class='text-decoration-none mr-2'
+                                        href="{{  route('plantilla-of-personnel.index') }}">
                                         Plantilla of Personnel
                                     </a>
                                 </li>
@@ -316,25 +319,28 @@
                                     </a>
                                 </li>
                                 <li class="submenu">
-									<a  class='text-decoration-none' href="javascript:void(0);"> <span>Salary Adjustment</span> <span class="menu-arrow"></span></a>
-									<ul style="display: none;">
-										<li>
-                                            <a class='text-decoration-none' href="{{  route('salary-adjustment.index') }}">
+                                    <a class='text-decoration-none' href="javascript:void(0);"> <span>Salary
+                                            Adjustment</span> <span class="menu-arrow"></span></a>
+                                    <ul style="display: none;">
+                                        <li>
+                                            <a class='text-decoration-none'
+                                                href="{{  route('salary-adjustment.index') }}">
                                                 <span>Individual</span>
                                             </a>
-                                            <a class='text-decoration-none' href="{{  route('salary-adjustment-per-office.index') }}">
+                                            <a class='text-decoration-none'
+                                                href="{{  route('salary-adjustment-per-office.index') }}">
                                                 <span>Per Office</span>
                                             </a>
                                         </li>
-									</ul>
-								</li>
-							</ul>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-		<!-- /Sidebar -->
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- /Sidebar -->
         <!-- Page Wrapper -->
         <div class="page-wrapper">
             <!-- Page Content -->
@@ -363,9 +369,14 @@
     <!-- /Main Wrapper -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="{{ asset('/assets/js/jquery.slimscroll.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-slimscroll@1.3.8/jquery.slimscroll.min.js"></script>
     <script src="{{ asset('/assets/js/app.js') }}"></script>
-    <script src="{{ asset('/assets/js/bootstrap.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
     @stack('page-scripts')
 </body>
 
