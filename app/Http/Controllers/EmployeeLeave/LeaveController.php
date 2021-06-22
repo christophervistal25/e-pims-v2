081e9250353c7776097b53ab8f172c443426faf2
paubaron <baron.paulajane@gmail.com>
2021-06-22 11:38:50 +0800

    Edit leave controller

--- app/Http/Controllers/EmployeeLeave/LeaveController.php
+++ app/Http/Controllers/EmployeeLeave/LeaveController.php
@@ -7,18 +7,9 @@ use Illuminate\Http\Request;
 
 class LeaveController extends Controller
 {
-    public function index()
-    {
-        return view('leave.leave-starting-balance');
-    }
-    
     public function show()
     {
         return view('leave.leave-application');
     }
 
-    public function create()
-    {
-        return view('leave.leave-monitoring');
-    }
 }
