2ae9507440c54851c1d1426c7968a2042f6a86e4
CHRis <christophervistal25@gmail.com>
2021-06-23 10:47:30 +0800

    Revised employee trap validation rule

--- app/Http/Requests/Employee/OldEmployeeUpdateRequest.php
+++ app/Http/Requests/Employee/OldEmployeeUpdateRequest.php
@@ -24,23 +24,25 @@ class OldEmployeeUpdateRequest extends FormRequest
      */
     public function rules()
     {
+
         $rules = [
-            'employee_id'      => 'required|exists:employees',
-            'firstName'        => ['required', new UpdateTrapfullname()],
-            'middleName'       => ['nullable', new UpdateTrapfullname()],
-            'lastName'         => ['required', new UpdateTrapfullname()],
-            'extension' => ['nullable', new UpdateTrapfullname()],
-            'dateOfBirth'      => ['required', 'date', new UpdateTrapfullname()],
-            'designation.position_code'      => 'required|exists:positions,position_code',
+            'employee_id'                  => 'required|exists:employees',
+            'firstName'                    => ['required', new UpdateTrapfullname()],
+            'middleName'                   => ['nullable', new UpdateTrapfullname()],
+            'lastName'                     => ['required', new UpdateTrapfullname()],
+            'extension'                    => ['nullable', new UpdateTrapfullname()],
+            'dateOfBirth'                  => ['required', 'date', new UpdateTrapfullname()],
+            'designation.position_code'    => 'required|exists:positions,position_code',
             'officeAssignment.office_code' => 'required|exists:offices,office_code',
-            'employmentStatus.stat_code' => 'required|exists:ref_statuses,stat_code',
-            'pagibigMidNo'     => 'nullable',
-            'philhealthNo'     => 'nullable',
-            'sssNo'            => 'nullable|unique:employees,sss_no,'. request('employee_id') . ',employee_id',
-            'tinNo'            => 'nullable|unique:employees,tin_no,'. request('employee_id') . ',employee_id',
+            'employmentStatus'             => 'required',
+            'employmentStatus.stat_code'   => 'required_with:employmentStatus|exists:ref_statuses,stat_code',
+            'pagibigMidNo'                 => 'nullable',
+            'philhealthNo'                 => 'nullable',
+            'sssNo'                        => 'nullable|unique:employees,sss_no,'. request('employee_id') . ',employee_id',
+            'tinNo'                        => 'nullable|unique:employees,tin_no,'. request('employee_id') . ',employee_id',
         ];
 
-            if(request()->employmentStatus['status_name'] === 'PERMANENT') {
+        if(!is_null(request()->employmentStatus) && request()->employmentStatus['status_name'] === 'PERMANENT') {
                 if(request()->dbpAccountNo !== '*') {
                     $rules['dbpAccountNo'] = 'unique:employees,dbp_account_no,' . request()->employee_id . ',employee_id';
                 } else {
