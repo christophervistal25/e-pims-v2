6d0ca0719010de7df9ae140430eaa547015434f1
dariel1990 <darielbongabong90@gmail.com>
2021-08-10 13:30:48 +0800

    dashboard fix

--- resources/views/blank-page.blade.php
+++ resources/views/blank-page.blade.php
@@ -1,5 +1,5 @@
 ﻿@extends('layouts.app')
-@section('title', 'Welcome User')
+@section('title', 'Welcome Admin!')
 @prepend('page-css')
 {{-- CSS HERE --}}
 <link rel="stylesheet" href="assets/plugins/morris/morris.css">
@@ -15,82 +15,132 @@
 </style>
 @endprepend
 @section('content')
-<div class="col-lg-12">
-    <div class="card-group m-b-30">
-        <div class="card">
+<div class="row">
+    <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
+        <div class="card dash-widget">
             <div class="card-body">
-                <div class="d-flex justify-content-between mb-3">
-                    <div>
-                        <span class="d-block">New Employees</span>
-                    </div>
-                    <div>
-                        <span class="text-success">+10%</span>
-                    </div>
+                <span class="dash-widget-icon"><i class="la la-list"></i></span>
+                <div class="dash-widget-info">
+                    <h3>112</h3>
+                    <h5><a href="#" class="badge badge-pill badge-light">Leaves Today</a></h5>
                 </div>
-                <h3 class="mb-3">10</h3>
-                <div class="progress mb-2" style="height: 5px;">
-                    <div class="progress-bar bg-primary" role="progressbar" style="width: 70%;" aria-valuenow="40"
-                        aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <p class="mb-0">Overall Employees 218</p>
             </div>
         </div>
-        <div class="card">
+    </div>
+    <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
+        <div class="card dash-widget">
             <div class="card-body">
-                <div class="d-flex justify-content-between mb-3">
-                    <div>
-                        <span class="d-block">Earnings</span>
-                    </div>
-                    <div>
-                        <span class="text-success">+12.5%</span>
-                    </div>
+                <span class="dash-widget-icon"><i class="la la-bookmark"></i></span>
+                <div class="dash-widget-info">
+                    <h3>44</h3>
+                    <h5><a href="#" class="badge badge-pill badge-light">New Plantillas</a></h5>
                 </div>
-                <h3 class="mb-3">$1,42,300</h3>
-                <div class="progress mb-2" style="height: 5px;">
-                    <div class="progress-bar bg-primary" role="progressbar" style="width: 70%;" aria-valuenow="40"
-                        aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <p class="mb-0">Previous Month <span class="text-muted">$1,15,852</span></p>
             </div>
         </div>
-        <div class="card">
+    </div>
+    <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
+        <div class="card dash-widget">
             <div class="card-body">
-                <div class="d-flex justify-content-between mb-3">
-                    <div>
-                        <span class="d-block">Expenses</span>
-                    </div>
-                    <div>
-                        <span class="text-danger">-2.8%</span>
-                    </div>
+                <span class="dash-widget-icon"><i class="fas fa-user-tie"></i></span>
+                <div class="dash-widget-info">
+                    <h3>37</h3>
+                    <h5><a href="#" class="badge badge-pill badge-light">Employees with Eligibility</a></h5>
                 </div>
-                <h3 class="mb-3">$8,500</h3>
-                <div class="progress mb-2" style="height: 5px;">
-                    <div class="progress-bar bg-primary" role="progressbar" style="width: 70%;" aria-valuenow="40"
-                        aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <p class="mb-0">Previous Month <span class="text-muted">$7,500</span></p>
             </div>
         </div>
-        <div class="card">
+    </div>
+    <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
+        <div class="card dash-widget">
             <div class="card-body">
-                <div class="d-flex justify-content-between mb-3">
-                    <div>
-                        <span class="d-block">Profit</span>
-                    </div>
-                    <div>
-                        <span class="text-danger">-75%</span>
-                    </div>
+                <span class="dash-widget-icon"><i class="fa fa-users"></i></span>
+                <div class="dash-widget-info">
+                    <h3>{{ $allEmployees }}</h3>
+                    <h5><a href="#" class="badge badge-pill badge-light">Employees</a></h5>
                 </div>
-                <h3 class="mb-3">$1,12,000</h3>
-                <div class="progress mb-2" style="height: 5px;">
-                    <div class="progress-bar bg-primary" role="progressbar" style="width: 70%;" aria-valuenow="40"
-                        aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <p class="mb-0">Previous Month <span class="text-muted">$1,42,000</span></p>
             </div>
         </div>
     </div>
 </div>
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card-group m-b-30">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between mb-3">
+                        <div>
+                            <span class="d-block">No. of Job Orders (Current)</span>
+                        </div>
+                        <div>
+                            <span class="text-success">{{ intval($no_of_jobOrder)/intval($allEmployees) * 100 }}%</span>
+                        </div>
+                    </div>
+                    <h3 class="mb-3">{{ $no_of_jobOrder }}</h3>
+                    <div class="progress mb-2" style="height: 5px;">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: {{ intval($no_of_jobOrder)/intval($allEmployees) * 100 }}%;" aria-valuenow="40"
+                            aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <p class="mb-0">Overall Employees {{ $allEmployees }}</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between mb-3">
+                        <div>
+                            <span class="d-block">No. of Regular Employees (Current)</span>
+                        </div>
+                        <div>
+                            <span class="text-success">{{ intval($no_of_regular)/intval($allEmployees) * 100 }}%</span>
+                        </div>
+                    </div>
+                    <h3 class="mb-3">{{ $no_of_regular }}</h3>
+                    <div class="progress mb-2" style="height: 5px;">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: {{ intval($no_of_regular)/intval($allEmployees) * 100 }}%;" aria-valuenow="40"
+                            aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <p class="mb-0">Overall Employees {{ $allEmployees }}</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between mb-3">
+                        <div>
+                            <span class="d-block">No. of Employees Promoted this year</span>
+                        </div>
+                        <div>
+                            <span class="text-success">{{ intval($no_of_promoted)/intval($allEmployees) * 100 }}%</span>
+                        </div>
+                    </div>
+                    <h3 class="mb-3">{{ $no_of_promoted }}</h3>
+                    <div class="progress mb-2" style="height: 5px;">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: {{ intval($no_of_promoted)/intval($allEmployees) * 100 }}%;" aria-valuenow="40"
+                            aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <p class="mb-0">Previous Year {{ $no_of_promoted_prev }}<span class="text-muted"></span></p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between mb-3">
+                        <div>
+                            <span class="d-block">No. of Active Employees</span>
+                        </div>
+                        <div>
+                            <span class="text-success">{{ intval($no_of_active)/intval($allEmployees) * 100 }}%</span>
+                        </div>
+                    </div>
+                    <h3 class="mb-3">{{ $no_of_active }}</h3>
+                    <div class="progress mb-2" style="height: 5px;">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: {{ intval($no_of_active)/intval($allEmployees) * 100 }}%;" aria-valuenow="40"
+                            aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <p class="mb-0">Inactive Employees {{ $no_of_inactive }}<span class="text-muted"></span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="row">
     <div class="col-lg-6">
         <div class="card">
