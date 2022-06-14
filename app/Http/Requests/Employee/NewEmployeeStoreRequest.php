4cdda4a959b8feb48c4a8f1786353a76bb87f46b
CHRis <christophervistal25@gmail.com>
2022-06-14 08:32:07 +0800

    Remove bank details field

--- app/Http/Requests/Employee/NewEmployeeStoreRequest.php
+++ app/Http/Requests/Employee/NewEmployeeStoreRequest.php
@@ -7,73 +7,54 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class NewEmployeeStoreRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
+      /**
+       * Determine if the user is authorized to make this request.
+       *
+       * @return bool
+       */
+      public function authorize()
+      {
+            return true;
+      }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        $rules = [
-            'firstName'                    => ['required', new StoreTrapfullname()],
-            'middleName'                   => ['nullable', new StoreTrapfullname()],
-            'lastName'                     => ['required', new StoreTrapfullname()],
-            'extension'                    => ['nullable', new StoreTrapfullname()],
-            'dateOfBirth'                  => ['required', 'date', new StoreTrapfullname()],
-            'designation.position_code'    => 'required|exists:positions,position_code',
-            'officeAssignment.office_code' => 'required|exists:offices,office_code',
-            'employmentStatus'             => 'required',
-            'employmentStatus.stat_code'   => 'required_with:employmentStatus|exists:ref_statuses,stat_code',
-            'pagibigMidNo'                 => 'nullable',
-            'philhealthNo'                 => 'nullable',
-            'sssNo'                        => 'nullable|unique:employees,sss_no',
-            'tinNo'                        => 'nullable|unique:employees,tin_no',
-            'username'                     => 'required|unique:users,username',
-            'email'                        => 'required|unique:users,email',
-            'password'                     => 'required|min:6|max:16|same:retypePassword',
-            'retypePassword'               => 'required|min:6|max:16',
-        ];
-        
-        if(request()->employmentStatus && request()->employmentStatus['status_name'] === 'PERMANENT') {
-            // Rule for first day of service as permanent.
-            $rules['firstDayOfService'] = 'required|date|before:' . date('Y-m-d');
-            // Ignore rules if the user enter an asterisk (*)
-            if(request()->dbpAccountNo === '*') {
-                $rules['dbpAccountNo'] = 'required';
-            } else {
-                $rules['dbpAccountNo'] = 'required|unique:employees,dbp_account_no';
-            }
-        } else {
-            if(request()->lbpAccountNo === '*') {
-                $rules['lbpAccountNo'] = 'required';
-            } else {
-                $rules['lbpAccountNo'] = 'required|unique:employees,lbp_account_no';
-            }
-        }
+      /**
+       * Get the validation rules that apply to the request.
+       *
+       * @return array
+       */
+      public function rules()
+      {
+            $rules = [
+                  'firstName'                    => ['required', new StoreTrapfullname()],
+                  'middleName'                   => ['nullable', new StoreTrapfullname()],
+                  'lastName'                     => ['required', new StoreTrapfullname()],
+                  'extension'                    => ['nullable', new StoreTrapfullname()],
+                  'dateOfBirth'                  => ['required', 'date', new StoreTrapfullname()],
+                  'designation.position_code'    => 'required|exists:positions,position_code',
+                  'officeAssignment.office_code' => 'required|exists:offices,office_code',
+                  'employmentStatus'             => 'required',
+                  'employmentStatus.stat_code'   => 'required_with:employmentStatus|exists:ref_statuses,stat_code',
+                  'pagibigMidNo'                 => 'nullable',
+                  'philhealthNo'                 => 'nullable',
+                  'sssNo'                        => 'nullable|unique:employees,sss_no',
+                  'tinNo'                        => 'nullable|unique:employees,tin_no',
+                  'username'                     => 'required|unique:users,username',
+                  'email'                        => 'required|unique:users,email',
+                  'password'                     => 'required|min:6|max:16|same:retypePassword',
+                  'retypePassword'               => 'required|min:6|max:16',
+            ];
 
 
-        return $rules;
-    }
+            return $rules;
+      }
 
-    public function attributes()
-    {
-        return [
-            'employmentStatus.stat_code'   => 'employment status',
-            'designation.position_code'    => 'designation',
-            'officeAssignment.office_code' => 'office',
-            'lbpAccountNo' => 'LBP account no.',
-            'dbpAccountNo' => 'DBP account no.',
-            'retypePassword' => 'password confirmation'
-        ];
-    }
+      public function attributes()
+      {
+            return [
+                  'employmentStatus.stat_code'   => 'employment status',
+                  'designation.position_code'    => 'designation',
+                  'officeAssignment.office_code' => 'office',
+                  'retypePassword' => 'password confirmation'
+            ];
+      }
 }
