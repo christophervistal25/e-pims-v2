50dc46895597466d118b2738a0859d99d2c6f0c5
CHRis <christophervistal25@gmail.com>
2022-07-20 09:53:58 +0800

    Add constants and division relation

--- app/Plantilla.php
+++ app/Plantilla.php
@@ -2,110 +2,110 @@
 
 namespace App;
 
-use App\Office;
-use App\Employee;
-use App\StepIncrement;
-use App\PlantillaPosition;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Plantilla extends Model
 {
-      // public $connection = 'DTR_PAYROLL_CONNECTION';
-      public $table = 'plantillas';
-      public const REGIONS = [
-            'Region 1',
-            'Region 2',
-            'Region 3',
-            'Region 4',
-            'Region 5',
-            'Region 6',
-            'Region 7',
-            'Region 8',
-            'Region 9',
-            'Region 10',
-            'Region 11',
-            'Region 12',
-            'NCR',
-            'CAR',
-            'CARAGA',
-            'ARMM',
-      ];
+    // public $connection = 'DTR_PAYROLL_CONNECTION';
+    public $table = 'plantillas';
 
-      public const AREA_TYPES = [
-        'Region', 'Province', 'District', 'Municipality', 'Foreign Post'
-      ];
+    public const REGIONS = [
+        'Region 1',
+        'Region 2',
+        'Region 3',
+        'Region 4',
+        'Region 5',
+        'Region 6',
+        'Region 7',
+        'Region 8',
+        'Region 9',
+        'Region 10',
+        'Region 11',
+        'Region 12',
+        'NCR',
+        'CAR',
+        'CARAGA',
+        'ARMM',
+    ];
 
-      public const AREA_LEVELS = ['K', 'T', 'S', 'A'];
+    public const AREA_TYPES = [
+        'Region', 'Province', 'District', 'Municipality', 'Foreign Post',
+    ];
 
-      protected $fillable = [
-            'plantilla_id',
-            'old_item_no',
-            'item_no',
-            'pp_id',
-            'sg_no',
-            'step_no',
-            'salary_amount',
-            'employee_id',
-            'area_code',
-            'area_type',
-            'area_level',
-            'date_original_appointment',
-            'date_last_promotion',
-            'office_code',
-            'division_id',
-            'status',
-            'year',
-      ];
+    public const AREA_LEVELS = ['K', 'T', 'S', 'A'];
 
-      protected $primaryKey = 'plantilla_id';
+    protected $fillable = [
+        'plantilla_id',
+        'old_item_no',
+        'item_no',
+        'pp_id',
+        'sg_no',
+        'step_no',
+        'salary_amount',
+        'employee_id',
+        'area_code',
+        'area_type',
+        'area_level',
+        'date_original_appointment',
+        'date_last_promotion',
+        'office_code',
+        'division_id',
+        'status',
+        'year',
+    ];
 
+    protected $primaryKey = 'plantilla_id';
 
-      public function __construct()
-      {
-            $this->table = DB::connection($this->connection)->getDatabaseName() . '.dbo.' . $this->getTable();
-      }
+    public function __construct()
+    {
+        $this->table = DB::connection($this->connection)->getDatabaseName().'.dbo.'.$this->getTable();
+    }
 
-      public function Employee()
-      {
-            return $this->belongsTo(Employee::class, 'employee_id', 'Employee_id')->select('Employee_id', 'FirstName', 'MiddleName', 'LastName', 'Suffix', 'OfficeCode', 'Birthdate', 'Work_Status');
-      }
+    public function Employee()
+    {
+        return $this->belongsTo(Employee::class, 'employee_id', 'Employee_id')->select('Employee_id', 'FirstName', 'MiddleName', 'LastName', 'Suffix', 'OfficeCode', 'Birthdate', 'Work_Status');
+    }
 
-      public function employee_record()
-      {
-            return $this->hasOne(Employee::class, 'Employee_id', 'employee_id');
-      }
+    public function employee_record()
+    {
+        return $this->hasOne(Employee::class, 'Employee_id', 'employee_id');
+    }
 
-      public function office()
-      {
-            return $this->hasOne(Office::class, 'office_code', 'office_code');
-      }
+    public function office()
+    {
+        return $this->hasOne(Office::class, 'office_code', 'office_code');
+    }
 
-      public function plantilla_positions()
-      {
-            return $this->hasOne(PlantillaPosition::class, 'pp_id', 'pp_id');
-      }
+    public function plantilla_positions()
+    {
+        return $this->hasOne(PlantillaPosition::class, 'pp_id', 'pp_id');
+    }
 
+    public function position()
+    {
+        return $this->hasOne(Position::class, 'PosCode', 'PosCode');
+    }
 
-      public function position()
-      {
-          return $this->hasOne(Position::class, 'PosCode', 'PosCode');
-      }
+    public function salary_adjustment()
+    {
+        $this->primaryKey = 'employee_id';
 
-      public function salary_adjustment()
-      {
-            $this->primaryKey = 'employee_id';
-            return $this->hasMany(SalaryAdjustment::class, 'employee_id', 'employee_id');
-      }
+        return $this->hasMany(SalaryAdjustment::class, 'employee_id', 'employee_id');
+    }
 
-      public function PlantillaSchedule()
-      {
-            return $this->belongsTo(PlantillaSchedule::class, 'plantilla_id', 'plantilla_id');
-      }
+    public function PlantillaSchedule()
+    {
+        return $this->belongsTo(PlantillaSchedule::class, 'plantilla_id', 'plantilla_id');
+    }
 
+    public function step()
+    {
+        return $this->hasOne(StepIncrement::class, 'employee_id', 'employee_id');
+    }
 
-      public function step()
-      {
-            return $this->hasOne(StepIncrement::class, 'employee_id', 'employee_id');
-      }
+    public function division()
+    {
+        return $this->hasOne(Division::class, 'division_id', 'division_id');
+    }
 }
