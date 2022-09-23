47603f593fa136437f1e165b56a7805bec4d5f20
CHRis <christophervistal25@gmail.com>
2022-09-23 14:48:41 +0800

    Add office service

--- app/Services/OfficeService.php
+++ app/Services/OfficeService.php
@@ -2,24 +2,25 @@
 
 namespace App\Services;
 
+use App\Office;
 use App\Office2;
 use App\OfficeCharging;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Collection;
 
 class OfficeService
 {
+    public function offices() :Collection
+    {
+        return Office::get();
+    }
+
     public function get(): Collection
     {
-        return Cache::rememberForever('OFFICES', function () {
-            return OfficeCharging::get();
-        });
+        return OfficeCharging::get();
     }
 
     public function office2(): Collection
     {
-        return Cache::rememberForever('OFFICES_2', function () {
-            return Office2::get();
-        });
+        return Office2::get();
     }
 }
