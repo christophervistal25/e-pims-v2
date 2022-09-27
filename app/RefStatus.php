e488390d71b3587458d58e90d13bce77cc4e3dc0
CHRis <christophervistal25@gmail.com>
2022-09-27 23:07:39 +0800

    remove static boot

--- app/RefStatus.php
+++ app/RefStatus.php
@@ -3,38 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Cache;
 
 class RefStatus extends Model
 {
     protected $fillable = ['status_name'];
-
-    public static function boot()
-    {
-        parent::boot();
-        self::creating(function ($status) {
-            $maxStatusCode = self::max('stat_code');
-            $code = str_pad(($maxStatusCode + 1), 4, '0', STR_PAD_LEFT);
-            $status->stat_code = $code;
-            Cache::forget('status');
-        });
-
-        self::created(function () {
-            Cache::forget('status');
-        });
-
-        self::updated(function () {
-            Cache::forget('status');
-        });
-
-        self::saved(function () {
-            Cache::forget('status');
-        });
-
-        self::deleted(function () {
-            Cache::forget('status');
-        });
-    }
 
     /**
      * Set the reference status name to uppercase
