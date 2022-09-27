a7b8b2725106e0856c84aadb3fbde02589361758
CHRis <christophervistal25@gmail.com>
2022-09-27 23:08:18 +0800

    remove other console commands

--- routes/console.php
+++ routes/console.php
@@ -1,43 +1,11 @@
 <?php
 
-use App\Employee;
-use App\Jobs\LeaveIncrementJob;
-use App\Plantilla;
-use App\PlantillaPosition;
-use App\Promotion;
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->describe('Display an inspiring quote');
 
-Artisan::command('increment', function () {
-    $employees = Employee::without(['position', 'office_charging', 'office_assignment', 'office_charging.desc'])
-        ->with(['leave_records' => function ($query) {
-            $query->orderBy('date_record', 'DESC');
-        }])
-        ->permanent()
-        ->active()
-        ->get(['Employee_id', 'FirstName', 'MiddleName', 'LastName', 'Suffix']);
-    LeaveIncrementJob::dispatch($employees)->delay(now()->addSeconds(1));
-});
 
 Artisan::command('clear', function () {
     // Plantilla::truncate();
     // PlantillaPosition::truncate();
     // Promotion::truncate();
-});
-
-Artisan::command('files_dump', function () {
-    $generatedServiceRecords = glob(storage_path().'\\'.'files\\*');
-    $generatedPds = glob(storage_path().'\\'.'generated_pds\\*');
-
-    foreach ($generatedServiceRecords as $generated) {
-        unlink($generated);
-    }
-
-    foreach ($generatedPds as $generated) {
-        unlink($generated);
-    }
 });
